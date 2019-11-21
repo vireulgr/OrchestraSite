cc8ad0ae9218b71ffd1a8e0e054eb1af5dd2fee0
George <grreulvi@gmail.com>
2019-11-21 20:29:26 +0300

    Replaced default react stub page with our own stubs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='container'>
+      <Header />
       <div className='jumbotron'>
-      <h1>Сайт оркестра СПб</h1>
-      <p>Текст для подзаголовка</p>
+        <h1>Сайт оркестра СПб</h1>
+        <p>Текст для подзаголовка</p>
       </div>
+      <Footer />
     </div>
   );
 }
