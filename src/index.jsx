821a876a0180237ffaf762dee22a409357171919
George <grreulvi@gmail.com>
2019-12-02 16:04:01 +0300

    [news_component] satisfying eslint

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 ReactDOM.render((<BrowserRouter><App /></BrowserRouter>), document.getElementById('root'));
